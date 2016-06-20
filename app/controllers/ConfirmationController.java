7426c53333bce8c81659850125db316d63f64b77
Rishabh Joshi <rishabh9@gmail.com>
2016-06-20 11:02:52 +0530

    Update confirmation controller

--- app/controllers/ConfirmationController.java
+++ app/controllers/ConfirmationController.java
@@ -1,15 +1,15 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.mongodb.WriteResult;
 import controllers.helpers.MailChimpAuthorizeCall;
 import daos.ConfirmationDao;
 import models.Confirmation;
-import org.springframework.util.StringUtils;
 import play.Logger;
-import play.data.DynamicForm;
 import play.data.FormFactory;
 import play.i18n.Messages;
 import play.i18n.MessagesApi;
+import play.libs.Json;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -28,30 +28,30 @@ public class ConfirmationController extends Controller {
     private final ConfirmationDao confirmationDao;
     private final MessagesApi messagesApi;
     private final MailChimpAuthorizeCall authorizeCall;
-    private final FormFactory formFactory;
 
     @Inject
     public ConfirmationController(final ConfirmationDao confirmationDao, final MessagesApi messagesApi,
-                                  final MailChimpAuthorizeCall authorizeCall, final FormFactory formFactory) {
+                                  final MailChimpAuthorizeCall authorizeCall) {
         super();
         this.confirmationDao = confirmationDao;
         this.messagesApi = messagesApi;
         this.authorizeCall = authorizeCall;
-        this.formFactory = formFactory;
     }
 
-    @BodyParser.Of(BodyParser.FormUrlEncoded.class)
+    @BodyParser.Of(BodyParser.Json.class)
     public Result confirm() {
-        final DynamicForm requestData = formFactory.form().bindFromRequest();
-        final String uid = requestData.get("uid");
-        final String timestamp = requestData.get("ts");
-        if (!StringUtils.hasText(uid) || !StringUtils.hasText(timestamp)) {
-            log.error("Error in parsing the confirmation data! Received: {} & {}", uid, timestamp);
+        JsonNode json = request().body().asJson();
+        Confirmation data;
+        try {
+            data = Json.fromJson(json, Confirmation.class);
+            log.debug("Received confirmation: {}", data.toString());
+        } catch (Exception e) {
+            log.error("Error in parsing the confirmation data!", e);
+            log.error("Received the following data: {}", json.toString());
             return badRequest();
         }
-        log.debug("Received installation confirmation from Xola: UID: {}, TS: {}", uid, timestamp);
 
-        final WriteResult result = confirmationDao.insert(new Confirmation(uid, timestamp));
+        WriteResult result = confirmationDao.insert(data);
         if (result.wasAcknowledged()) {
             final String upsertedId = result.getUpsertedId().toString();
             log.info("Confirmation {} created", upsertedId);
--- app/models/Confirmation.java
+++ app/models/Confirmation.java
@@ -7,28 +7,23 @@ import java.util.Objects;
  */
 public class Confirmation extends BaseModel {
 
-    private String uid;
-    private String ts;
+    private String pluginId;
+    private Seller seller;
 
-    public Confirmation(String uid, String ts) {
-        this.uid = uid;
-        this.ts = ts;
+    public String getPluginId() {
+        return pluginId;
     }
 
-    public String getUid() {
-        return uid;
+    public void setPluginId(String pluginId) {
+        this.pluginId = pluginId;
     }
 
-    public void setUid(String uid) {
-        this.uid = uid;
+    public Seller getSeller() {
+        return seller;
     }
 
-    public String getTs() {
-        return ts;
-    }
-
-    public void setTs(String ts) {
-        this.ts = ts;
+    public void setSeller(Seller seller) {
+        this.seller = seller;
     }
 
     @Override
@@ -36,20 +31,87 @@ public class Confirmation extends BaseModel {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Confirmation that = (Confirmation) o;
-        return Objects.equals(uid, that.uid) &&
-                Objects.equals(ts, that.ts);
+        return Objects.equals(pluginId, that.pluginId) &&
+                Objects.equals(seller, that.seller);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(uid, ts);
+        return Objects.hash(pluginId, seller);
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Confirmation-{");
-        sb.append("uid='").append(uid).append('\'');
-        sb.append(", ts='").append(ts).append('\'');
+        final StringBuilder sb = new StringBuilder("Confirmation{");
+        sb.append("pluginId='").append(pluginId).append('\'');
+        sb.append(", seller=").append(seller);
+        sb.append('}');
+        return sb.toString();
+    }
+}
+
+class Seller {
+
+    private String id;
+    private String name;
+    private String email;
+    private String company;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(id, seller.id) &&
+                Objects.equals(name, seller.name) &&
+                Objects.equals(email, seller.email) &&
+                Objects.equals(company, seller.company);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, company);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Seller{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", company='").append(company).append('\'');
         sb.append('}');
         return sb.toString();
     }
