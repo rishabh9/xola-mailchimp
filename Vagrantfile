058a850c60821af75d050c99a7e7852be2b52a5a
Rishabh Joshi <rishabh9@gmail.com>
2016-07-19 15:17:43 +0530

    Added Vagrant configuration

---
+++ Vagrantfile
@@ -0,0 +1,70 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  # The most common configuration options are documented and commented below.
+  # For a complete reference, please see the online documentation at
+  # https://docs.vagrantup.com.
+
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://atlas.hashicorp.com/search.
+  config.vm.box = "ubuntu/trusty64"
+
+  # The url from where the 'config.vm.box' box will be fetched if it
+  # doesn't already exist on the user's system.
+  # http://www.vagrantbox.es/
+  # config.vm.box_url = "http://files.vagrantup.com/trusty64.box"
+
+  if Vagrant.has_plugin?("vagrant-cachier")
+      # Configure cached packages to be shared between instances of the same base box.
+      # More info on http://fgrehm.viewdocs.io/vagrant-cachier/usage
+      config.cache.scope = :box
+  end
+
+  # config.vm.define "mailchimp" do |mailchimp|
+  # end
+
+  # configurating the vm
+  config.vm.provider "virtualbox" do |v|
+    v.name = "mailchimp"
+    # max 75% CPU cap
+    v.customize ["modifyvm", :id, "--cpuexecutioncap", "75"]
+    # give vm max 3GB ram
+    v.memory = 1024
+  end
+
+  # run "vagrant-machine-setup.sh" shell script when setting up our machine
+  config.vm.provision :shell, :privileged => false, :path => "vagrant-machine-setup.sh"
+  config.vm.provision :shell, :privileged => false, :path => "vagrant-machine-run.sh",run: "always"
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  config.vm.network :forwarded_port, guest: 9012, host: 9012, id: "mailchimp"
+  config.vm.network :forwarded_port, guest: 9912, host: 9912, id: "debug"
+  config.vm.network :forwarded_port, guest: 22,   host: 2212, id: "ssh"
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  config.vm.network :private_network, ip: "10.10.10.12"
+
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network :public_network
+
+  # If true, then any SSH connections made will enable agent forwarding.
+  # Default value: false
+  # config.ssh.forward_agent = true
+
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder. And the optional third
+  # argument is a set of non-required options.
+  # config.vm.synced_folder "../source/xola-mailchimp", "/mailchimp", create: true
+
+end
---
+++ vagrant-machine-setup.sh
@@ -0,0 +1,192 @@
+#!/usr/bin/env bash
+
+#variables
+activatorVersion="1.3.10"
+sbtVersion="0.13.11"
+
+echo "=========================================="
+echo "Provision VM START"
+echo "=========================================="
+
+sudo apt-get update
+
+###############################################
+# install prerequisits
+###############################################
+sudo apt-get -y -q upgrade
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get -y -q update
+sudo apt-get -y -q install software-properties-common htop
+sudo apt-get -y -q install build-essential
+sudo apt-get -y -q install tcl8.5
+
+###############################################
+# Install Java 8
+###############################################
+# sudo apt-get install -y openjdk-8-jdk
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get -y -q install oracle-java8-installer
+sudo update-java-alternatives -s java-8-oracle
+
+###############################################
+# In case you need Java 7
+###############################################
+# sudo apt-get install -y openjdk-7-jdk
+#echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+#sudo apt-get -y -q install oracle-java7-installer
+
+###############################################
+# Install Git
+###############################################
+# sudo apt-get -y install git
+
+###############################################
+# Install imagemagick
+###############################################
+#sudo apt-get -y install imagemagick
+
+###############################################
+# Install Scala
+###############################################
+# sudo apt-get -y install scala
+
+###############################################
+# Install Unzip
+###############################################
+sudo apt-get -y install unzip
+
+###############################################
+# Install NodeJS
+###############################################
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+sudo apt-get -y install nodejs
+ln -s /usr/bin/nodejs /user/bin/node
+# Add node_modules to environment variables
+echo "export NODE_PATH=/usr/local/lib/node_modules" >> ~/.bashrc
+
+###############################################
+# Install NPM
+###############################################
+sudo apt-get -y install npm
+
+###############################################
+# Install CoffeeScript
+###############################################
+# sudo npm install -g coffee-script
+
+###############################################
+# Install Bower
+###############################################
+# sudo npm install -g bower
+
+###############################################
+# Install Sass
+###############################################
+# sudo gem install sass
+
+###############################################
+# Install Redis
+# More info about it: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-redis
+###############################################
+# echo "Download Redis..."
+# wget http://download.redis.io/releases/redis-stable.tar.gz
+# tar xzf redis-stable.tar.gz
+# cd redis-stable
+# make
+# make test
+# sudo make install
+# cd utils
+# sudo ./install_server.sh
+# cd /home/vagrant/
+r# m redis-stable.tar.gz
+# echo "Redis done."
+
+###############################################
+# Install PostgreSQL
+###############################################
+# sudo apt-get -y install postgresql postgresql-contrib postgresql-client-common postgresql-common
+
+###############################################
+# Install SBT
+###############################################
+echo "Download SBT..."
+wget http://dl.bintray.com/sbt/debian/sbt-$sbtVersion.deb
+sudo dpkg -i sbt-$sbtVersion.deb
+sudo apt-get update
+sudo apt-get install sbt
+rm sbt-$sbtVersion.deb
+
+echo "SBT done."
+# Use node as default JavaScript Engine
+echo "export SBT_OPTS=\"\$SBT_OPTS -Dsbt.jse.engineType=Node\"" >> ~/.bashrc
+
+###############################################
+# Install typesafe activator
+###############################################
+cd /home/vagrant
+echo "Download Typesafe Activator..."
+wget http://downloads.typesafe.com/typesafe-activator/$activatorVersion/typesafe-activator-$activatorVersion.zip
+unzip -d /home/vagrant typesafe-activator-$activatorVersion.zip
+rm typesafe-activator-$activatorVersion.zip
+echo "Typesafe Activator done."
+# Add activator to environment variables
+echo "export PATH=/home/vagrant/activator-dist-$activatorVersion/bin:\$PATH" >> ~/.bashrc
+
+###############################################
+# Reset bash
+###############################################
+source ~/.bashrc
+
+###############################################
+# Install MongDB
+###############################################
+# echo "Download MongoDB..."
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+# echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+# sudo apt-get update
+# sudo apt-get -y install mongodb-org
+# echo "MongDB done."
+
+###############################################
+# Reset bash
+###############################################
+source ~/.bashrc
+
+###############################################
+# Show installation summary
+###############################################
+echo "=========================================="
+echo "Provision VM summary"
+echo "=========================================="
+echo "Dependencies installed:"
+echo " "
+echo "jdk version:"
+javac -version
+echo " "
+echo "NodeJS version:"
+node -v
+echo " "
+echo "NPM version"
+npm -v
+echo " "
+echo "CoffeeScript version:"
+coffee -v
+echo " "
+echo "Bower version:"
+bower -v
+echo " "
+echo "Sass version:"
+sass -v
+echo " "
+echo "Redis version"
+redis-server -v
+echo " "
+echo "PostgreSQL version"
+psql --version
+echo " "
+echo "mongoDB version"
+mongod --version
+echo " "
+echo "=========================================="
+echo "Provision VM finished"
+echo "=========================================="
