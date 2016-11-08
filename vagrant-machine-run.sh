a9423891c9aaa9b36daf0e4fbefdf46e87db9644
Rishabh Joshi <rishabh9@gmail.com>
2016-11-08 16:35:29 +0530

    Updated vagrant config

--- vagrant-machine-run.sh
+++ vagrant-machine-run.sh
@@ -2,13 +2,15 @@
 # Things that you need to run in every vagrant up
 
 # variables
-# activatorVersion="1.3.10"
+activatorVersion="1.3.10"
 
-# echo "=========================================="
-# echo "Set tasks to run"
-# echo "=========================================="
+echo "=========================================="
+echo "Set tasks to run"
+echo "=========================================="
 ###############################################
 # Get activator running
 ###############################################
-# cd /vagrant/
-# /home/vagrant/activator-dist-$activatorVersion/bin/activator run
+cd /vagrant/
+/home/vagrant/activator-dist-$activatorVersion/bin/activator clean compile universal:packageZipTarball
+sudo tar -xf /vagrant/target/universal/xola-mailchimp-*.tgz -C /var/xola/plugins/mailchimp --strip-components 1
+sudo service mailchimp restart
