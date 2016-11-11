997b20133858d18f1ba3565db25164559ef3a707
Rishabh Joshi <rishabh9@gmail.com>
2016-11-11 21:44:50 +0530

    Updated logger

--- app/ErrorHandler.java
+++ app/ErrorHandler.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.JsonMappingException;
 import play.Configuration;
 import play.Environment;
 import play.Logger;
@@ -8,16 +9,23 @@ import play.http.DefaultHttpErrorHandler;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Results;
+import utils.Errors;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import static play.mvc.Http.Status.*;
+
 /**
  * @author rishabh
  */
 public class ErrorHandler extends DefaultHttpErrorHandler {
+
+    private static final String UNEXPECTED_ERROR = "An unexpected error has occurred!";
+    private static final String JSON_ERROR = "Please ensure the request has a valid JSON.";
+    private static final String INVALID_REQUEST = "Unable to process your request. Please verify your request and try again.";
 
     private final Logger.ALogger log = Logger.of(ErrorHandler.class);
 
@@ -28,56 +36,77 @@ public class ErrorHandler extends DefaultHttpErrorHandler {
     }
 
     @Override
-    public CompletionStage<Result> onClientError(Http.RequestHeader request, int statusCode, String message) {
-        log.error("ERROR-HANDLER: Status Code: {}, Message: {}", statusCode, message);
-        return CompletableFuture.completedFuture(Results.badRequest(views.html.errors.render(message)));
-    }
-
-    @Override
-    protected CompletionStage<Result> onBadRequest(Http.RequestHeader request, String message) {
-        log.error("ERROR-HANDLER: Message: {}", message);
-        return CompletableFuture.completedFuture(Results.badRequest(views.html.errors.render(message)));
-    }
-
-    @Override
-    protected CompletionStage<Result> onForbidden(Http.RequestHeader request, String message) {
-        log.error("ERROR-HANDLER: Message: {}", message);
-        return CompletableFuture.completedFuture(Results.forbidden(views.html.errors.render(message)));
-    }
-
-    @Override
-    protected CompletionStage<Result> onNotFound(Http.RequestHeader request, String message) {
-        log.error("ERROR-HANDLER: Message: {}", message);
-        return CompletableFuture.completedFuture(Results.notFound(views.html.errors.render(message)));
-    }
-
-    @Override
-    protected CompletionStage<Result> onOtherClientError(Http.RequestHeader request, int statusCode, String message) {
-        log.error("ERROR-HANDLER: Status Code: {}, Message: {}", statusCode, message);
-        return CompletableFuture.completedFuture(Results.badRequest(views.html.errors.render(message)));
-    }
-
-    @Override
-    public CompletionStage<Result> onServerError(Http.RequestHeader request, Throwable exception) {
-        log.error("ERROR-HANDLER: Message: {}", exception.getMessage(), exception);
-        return CompletableFuture.completedFuture(Results.internalServerError(views.html.errors.render(exception.getMessage())));
-    }
-
-    @Override
-    protected void logServerError(Http.RequestHeader request, UsefulException exception) {
-        log.error("ERROR-HANDLER: Message: {}", exception.getMessage(), exception);
-        super.logServerError(request, exception);
-    }
-
-    @Override
     protected CompletionStage<Result> onDevServerError(Http.RequestHeader request, UsefulException exception) {
-        log.error("ERROR-HANDLER: Message: {}", exception.getMessage(), exception);
-        return super.onDevServerError(request, exception);
+        log.warn(exception.getMessage(), exception);
+        if (exception.getCause() instanceof JsonMappingException) {
+            return CompletableFuture.completedFuture(Results.badRequest(Errors.toJson(BAD_REQUEST, JSON_ERROR)));
+        } else {
+            return CompletableFuture.completedFuture(
+                    Results.internalServerError(Errors.toJson(INTERNAL_SERVER_ERROR, UNEXPECTED_ERROR)));
+        }
     }
 
     @Override
     protected CompletionStage<Result> onProdServerError(Http.RequestHeader request, UsefulException exception) {
-        log.error("ERROR-HANDLER: Message: {}", exception.getMessage(), exception);
-        return CompletableFuture.completedFuture(Results.internalServerError(views.html.errors.render(exception.getMessage())));
+        log.warn(exception.getMessage(), exception);
+        if (exception.getCause() instanceof JsonMappingException) {
+            return CompletableFuture.completedFuture(Results.badRequest(Errors.toJson(BAD_REQUEST, JSON_ERROR)));
+        } else {
+            return CompletableFuture.completedFuture(
+                    Results.internalServerError(Errors.toJson(INTERNAL_SERVER_ERROR, UNEXPECTED_ERROR)));
+        }
+    }
+
+    @Override
+    protected CompletionStage<Result> onOtherClientError(Http.RequestHeader request, int statusCode, String message) {
+        log.warn("Status: {}, Message: {}", statusCode, message);
+        return CompletableFuture.completedFuture(Results.status(statusCode, Errors.toJson(statusCode, INVALID_REQUEST)));
+    }
+
+    @Override
+    protected CompletionStage<Result> onNotFound(Http.RequestHeader request, String message) {
+        log.warn(message);
+        log.warn("{} - Requested: '{} {} {} {}', From: '{}'", message, request.method(), request.host(),
+                request.path(), request.contentType().get(), request.remoteAddress());
+        return CompletableFuture.completedFuture(Results.status(NOT_FOUND, Errors.toJson(NOT_FOUND, message)));
+    }
+
+    @Override
+    public CompletionStage<Result> onClientError(Http.RequestHeader request, int statusCode, String message) {
+        log.warn("Status: {}, Message: {}", statusCode, message);
+        return CompletableFuture.completedFuture(Results.status(statusCode, Errors.toJson(statusCode, INVALID_REQUEST)));
+    }
+
+    @Override
+    protected CompletionStage<Result> onBadRequest(Http.RequestHeader request, String message) {
+        log.warn(message);
+        log.warn("{} - Requested: '{} {} {} {}', From: '{}'", message, request.method(), request.host(),
+                request.path(), request.contentType().get(), request.remoteAddress());
+        return CompletableFuture.completedFuture(Results.status(BAD_REQUEST, Errors.toJson(BAD_REQUEST, message)));
+    }
+
+    @Override
+    protected CompletionStage<Result> onForbidden(Http.RequestHeader request, String message) {
+        log.warn(message);
+        log.warn("{} - Requested: '{} {} {} {}', From: '{}'", message, request.method(), request.host(),
+                request.path(), request.contentType().get(), request.remoteAddress());
+        return CompletableFuture.completedFuture(Results.status(FORBIDDEN, Errors.toJson(FORBIDDEN, message)));
+    }
+
+    @Override
+    public CompletionStage<Result> onServerError(Http.RequestHeader request, Throwable exception) {
+        log.warn(exception.getMessage(), exception);
+        if (exception.getCause() instanceof JsonMappingException) {
+            return CompletableFuture.completedFuture(Results.badRequest(Errors.toJson(BAD_REQUEST, JSON_ERROR)));
+        } else {
+            return CompletableFuture.completedFuture(
+                    Results.internalServerError(Errors.toJson(INTERNAL_SERVER_ERROR, UNEXPECTED_ERROR)));
+        }
+    }
+
+    @Override
+    protected void logServerError(Http.RequestHeader request, UsefulException usefulException) {
+        // TODO: Add code to log something useful here.
+        super.logServerError(request, usefulException);
     }
 }
